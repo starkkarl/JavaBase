5e9c6b5adf601700636b5af93bc57a5bf5a39ada
stark <stark.karl.program@gmail.com>
2021-08-18 12:51:43 +0800

    2021/08/18 单链表第二种方式根据排名添加及修改

--- javabase-code/DataStructures/src/main/java/com/star/linkedlist03/SingleLinkedListDemo.java
+++ javabase-code/DataStructures/src/main/java/com/star/linkedlist03/SingleLinkedListDemo.java
@@ -15,12 +15,30 @@ public class SingleLinkedListDemo {
         //创建一个链表
         SingleLinkedList singleLinkedList = new SingleLinkedList();
         //加入
-        singleLinkedList.add(hero1);
-        singleLinkedList.add(hero2);
-        singleLinkedList.add(hero3);
-        singleLinkedList.add(hero4);
-        //显示一把
+//        singleLinkedList.add(hero1);
+//        singleLinkedList.add(hero2);
+//        singleLinkedList.add(hero3);
+//        singleLinkedList.add(hero4);
+
+        //加入按照编号的顺序
+        singleLinkedList.addByOrder(hero1);
+        singleLinkedList.addByOrder(hero4);
+        singleLinkedList.addByOrder(hero2);
+        singleLinkedList.addByOrder(hero3);
+
+        //显示一下
         singleLinkedList.list();
+
+        //测试修改节点的代码
+        HeroNode newHeroNode = new HeroNode(2, "小卢", "玉麒麟~");
+        singleLinkedList.update(newHeroNode);
+
+        System.out.println("修改后的链表情况~~~");
+        singleLinkedList.list();
+
+
+        //显示一把
+//        singleLinkedList.list();
     }
 }
 
@@ -49,6 +67,69 @@ class SingleLinkedList{
         temp.next = heroNode;
     }
 
+    //第二种方式在添加英雄时，根据排名将英雄插入到指定位置
+    //(如果有这个排名，则添加失败，并给出提示)
+    public void addByOrder(HeroNode heroNode){
+        //因为头节点不能动，因此我们仍然通过一个辅助指针(变量)来帮助找到添加的位置
+        //因为单链表，因为我们找的temp，是位于 添加位置的前一个节点，否则插入不了
+        HeroNode temp = head;
+        boolean flag = false;//flag标志添加的编号是否存在，默认为false
+        while (true){
+            if(temp.next == null){//说明temp已经在链表的最后
+                break;
+            }
+            if(temp.next.no > heroNode.no) {//位置找到，就在temp的后面插入
+                break;
+            }else if(temp.next.no == heroNode.no){//说明希望添加的heroNode的编号已然存在
+                flag = true;//说明编号已存在
+                break;
+            }
+            temp = temp.next;//后移，遍历当前链表
+        }
+        //判断flag的值
+        if(flag) {//不能添加，说明编号存在
+            System.out.printf("准备插入的英雄的编号 %d 已经存在了，不能加入\n",heroNode.no);
+        }else{
+            //插入到链表中，temp的后面
+            heroNode.next = temp.next;
+            temp.next = heroNode;
+        }
+    }
+
+    //修改节点的信息，根据no编号来修改，即no编号不能改
+    //说明
+    //1.根据 newHeroNode 的no 来修改即可
+    public void update(HeroNode newHeroNode){
+        //判断是否为空
+        if(head.next == null){
+            System.out.println("链表为空~");
+            return;
+        }
+        //找到需要修改的节点，根据no编号
+        //定义一个辅助变量
+        HeroNode temp = head.next;
+        boolean flag = false;//表示是否找到该节点
+        while (true){
+            if(temp == null){
+                break;//已经遍历完链表
+            }
+            if(temp.no == newHeroNode.no){
+                //找到
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        //根据flag 判断是否找到要修改的节点
+        if(flag){
+            temp.name = newHeroNode.name;
+            temp.nickname = newHeroNode.nickname;
+        }else{//没有找到
+            System.out.printf("没有找到编号 %d 的节点，不能修改\n",newHeroNode.no);
+        }
+    }
+
+
     //显示链表[遍历]
     public void list(){
         //判断链表是否为空
@@ -69,6 +150,7 @@ class SingleLinkedList{
             temp = temp.next;
         }
     }
+
 }
 
 
