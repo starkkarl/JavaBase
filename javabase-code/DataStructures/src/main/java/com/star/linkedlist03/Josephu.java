16974ee163f819d0bfd72971fe4d3a92ad418060
stark <stark.karl.program@gmail.com>
2021-08-25 12:42:36 +0800

    2021/08/25 12:41 约瑟夫环形链表出圈问题处理

--- javabase-code/DataStructures/src/main/java/com/star/linkedlist03/Josephu.java
+++ javabase-code/DataStructures/src/main/java/com/star/linkedlist03/Josephu.java
@@ -11,6 +11,10 @@ public class Josephu {
         CircleSingleLinkedList circleSingleLinkedList = new CircleSingleLinkedList();
         circleSingleLinkedList.addBoy(5);//加入5个小孩节点
         circleSingleLinkedList.showBoy();
+
+        //测试小孩出圈是否正确
+        circleSingleLinkedList.countBoy(1,2,5);
+
     }
 }
 
@@ -59,6 +63,56 @@ class CircleSingleLinkedList{
             curBoy = curBoy.getNext();//curBoy后移
         }
     }
+
+
+    //根据用户的输入，计算出小孩出圈的顺序
+
+    /**
+     * @param startNo 表示从第几个小孩开始数数
+     * @param countNum 表示数几下
+     * @param nums 表示最初有多少小孩在圈中
+     */
+    public void countBoy (int startNo,int countNum,int nums){
+        //先对数据进行校验
+        if(first == null || startNo < 1 || startNo > nums){
+            System.out.println("参数输入有误，请重新输入！！~");
+            return;
+        }
+        //创建一个辅助指针帮助完成小孩出圈，
+        Boy helper = first;
+        //需求创建一个辅助指针(变量)helper，事先应该指向环形链表的最后这个节点
+        while (true){
+            if(helper.getNext() == first){//说明helper指向最后小孩节点
+                break;
+            }
+            helper = helper.getNext();
+        }
+        //小孩报数前，先让first和helper移动k-1次
+        for(int j = 0; j < startNo - 1;j++){
+            first = first.getNext();
+            helper = helper.getNext();
+        }
+        //当小孩报数时，让first和helper指针同时的移动m-1次，然后出圈
+        //这里是一个循环操作，直到圈中只有一个节点
+        while (true){
+            if(helper == first){//说明圈中只有一个节点
+                break;
+            }
+            //让 first 和 helper 指针同时的移动countNum - 1
+            for (int j = 0; j < countNum - 1; j++) {
+                first = first.getNext();
+                helper = helper.getNext();
+            }
+            //这时first指向的节点，就是要出圈的小孩节点
+            System.out.printf("小孩%d出圈\n",first.getNo());
+            //这时将first指向的小孩节点出圈
+            first = first.getNext();
+            helper.setNext(first);
+
+        }
+        System.out.printf("最后留在圈中的小孩编号%d \n",first.getNo());
+    }
+
 }
 
 //创建一个Boy类，表示一个节点
